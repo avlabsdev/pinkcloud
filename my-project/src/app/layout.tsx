725c1aeb9f70d42a7bb05dce2ffeb956959c92f5
Aaron Varga <hello@avlabs.dev>
2024-11-12 18:35:05 -0500

    metadata and favicon update

--- my-project/src/app/layout.tsx
+++ my-project/src/app/layout.tsx
@@ -21,6 +21,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* Add in opengraph data */}
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Pink Cloud" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={`${manrope.variable} antialiased`}>{children}</body>
     </html>
   );
