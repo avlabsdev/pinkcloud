985d62907b8efd8802b3e7f1466feedb1c2aa245
Aaron Varga <hello@avlabs.dev>
2024-11-12 10:42:51 -0500

    page updates

--- my-project/src/app/page.tsx
+++ my-project/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex flex-col grow py-16">
+      <main className="flex flex-col grow py-16 gap-32">
         <section className="flex items-center gap-16">
           <div className="w-2/5 flex flex-col gap-8">
             <span className="flex items-center p-3 px-10 border rounded-full border-white border-opacity-40 text-xl w-fit bg-white bg-opacity-5">
@@ -34,6 +34,12 @@ export default function Home() {
                 Join us in our mission to change the world one day at a time.
               </span>
             </div>
+          </div>
+        </section>
+        <section>
+          <div className="flex">
+            <div className="w-3/5">Together for a better tomorrow.</div>
+            <div className="w-3/5">Contributions make</div>
           </div>
         </section>
       </main>
